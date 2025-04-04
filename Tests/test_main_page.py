3d663d2ccb589ea2d4cdcf112d0e903e87095e1f
Boss of gym <naydanov.dmitriy@gmail.com>
2025-04-04 11:49:42 +0200

    update test main page

--- Tests/test_main_page.py
+++ Tests/test_main_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
+url = "http://selenium1py.pythonanywhere.com"
+
 def test_add_to_cart_button_exists(browser):
-    url = "http://selenium1py.pythonanywhere.com"
     browser.get(url)
+    go_to_login_page(browser)
+
+def go_to_login_page(browser):
     LOGIN_LINK = browser.find_element(By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK.click()
