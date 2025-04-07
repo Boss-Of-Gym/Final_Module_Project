b4255937b7da2000278fc5c1d45f7aa4c91e97ff
Boss of gym <naydanov.dmitriy@gmail.com>
2025-04-07 12:41:42 +0200

    update scripts for main page

--- Tests/Pages/main_page.py
+++ Tests/Pages/main_page.py
@@ -1,9 +1,12 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
+    
     def go_to_login_page(self):
         LOGIN_LINK = self.browser.find_element(By.CSS_SELECTOR,'#login_link')
         LOGIN_LINK.click()
+    
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "login link is not present"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "login link is not present"
