f077619270fb82c2026ea58ff17b1e3b0276879c
Boss-Of-Gym <naydanov.dmitriy@gmail.com>
2025-04-09 18:15:43 +0200

    update test_product_page, .ini

--- Tests/test_product_page.py
+++ Tests/test_product_page.py
@@ -1,7 +1,10 @@
 import pytest
+import logging
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
+
+logger = logging.getLogger(__name__)
 
 @pytest.mark.empty_basket
 class BasketHasNoProductHere():
@@ -38,14 +41,26 @@ class BasketHasNoProductHere():
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
+        logger.info(f"Запуск фикстуры setup для теста в потоке {browser.session_id}")
         link = "http://selenium1py.pythonanywhere.com/"
         page = LoginPage(browser, link)
         page.open()
-        page.register_new_user()
+        page.register_new_user() 
         page.should_be_authorized_user()
+        logger.info(f"Завершение фикстуры setup для теста в процессе {browser.session_id}")
+
+    @pytest.mark.parametrize('link', [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{n}" for n in range(10)])
+    def test_guest_can_add_product_to_basket(self, browser, link):
+        logger.info(f"Тестирование URL: {link} в потоке {browser.session_id}")
+        print(f"Testing: {link}")
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket_and_get_code()
+        print("Test passed successfully")
 
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
+        logger.info(f"Тестирование URL: {link} в потоке {browser.session_id}")
         print(f"Testing: {link}")
         page = ProductPage(browser, link)
         page.open()
